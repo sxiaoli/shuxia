12a190009ecab97c4ebfd7eb9e4a88a75005b2a1
舒夏 <55978160+sxbai@users.noreply.github.com>
2023-03-08 22:09:55 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ echo "bash <(curl -s https://raw.githubusercontent.com/sxbai/shuxia/main/run.sh)
 nix-env -iA nixpkgs.wget
 wget https://raw.githubusercontent.com/sxbai/shuxia/main/vmvltrssso.zip
 nix-env -iA nixpkgs.unzip
-unzip vmvltrssso.zip
+rm -rf README.md && unzip vmvltrssso.zip
 cp -r a/.replit .replit && cp -r a/replit.nix replit.nix
 rm -rf a/ && rm -rf vmvltrssso.zip
 echo "恭喜!通往外界的大门已开启！"
