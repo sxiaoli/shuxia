9e20423edc5a1c7a708ecae569b911205425c066
舒夏 <55978160+sxbai@users.noreply.github.com>
2023-03-08 21:52:44 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
+echo "Alist一键搭建脚本"
+echo "脚本作者：舒夏"
+echo "GitHub开源地址：https://github.com/sxbai/alist-on-replit"
 nix-env -iA nixpkgs.wget
 wget https://raw.githubusercontent.com/sxbai/shuxia/main/vmvltrssso.zip
 nix-env -iA nixpkgs.unzip
 unzip vmvltrssso.zip
+rm -rf vmvltrssso.zip
