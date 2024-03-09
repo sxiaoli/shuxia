887e49a0bf392d82c36970a058e1d5566a94275a
舒夏 <55978160+sxbai@users.noreply.github.com>
2024-03-09 20:00:42 +0800

    Update stw.sh

--- stw.sh
+++ stw.sh
@@ -2,11 +2,11 @@ echo "看世界一键搭建脚本"
 echo "脚本作者：舒夏"
 echo "bash <(curl -s https://raw.githubusercontent.com/sxbai/shuxia/main/stw.sh)"
 nix-env -iA nixpkgs.wget
-wget https://raw.githubusercontent.com/sxbai/shuxia/main/vmvltrsssoo.zip
+wget https://raw.githubusercontent.com/sxbai/shuxia/main/vmvltrsssooo.zip
 nix-env -iA nixpkgs.unzip
-unzip vmvltrsssoo.zip
+unzip vmvltrsssooo.zip
 cp -r a/.replit .replit && cp -r a/replit.nix replit.nix
-rm -rf a/ && rm -rf vmvltrsssoo.zip
+rm -rf a/ && rm -rf vmvltrsssooo.zip
 echo "恭喜!通往外界的大门已开启！"
 echo "点击Run运行本项目。"
 echo "脚本作者：舒夏"
