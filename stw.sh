6ec5ec670548efd344312ac8c52294cd325bab81
舒夏 <55978160+sxbai@users.noreply.github.com>
2024-04-08 18:39:29 +0800

    Update stw.sh

--- stw.sh
+++ stw.sh
@@ -1,12 +1,13 @@
 echo "看世界一键搭建脚本"
 echo "脚本作者：舒夏"
+echo "Xray-core v1.8.10"
 echo "bash <(curl -s https://raw.githubusercontent.com/sxbai/shuxia/main/stw.sh)"
 nix-env -iA nixpkgs.wget
-wget https://raw.githubusercontent.com/sxbai/shuxia/main/vmvltrsssooo.zip
+wget https://raw.githubusercontent.com/sxbai/shuxia/main/vmvltrssso.zip
 nix-env -iA nixpkgs.unzip
-unzip vmvltrsssooo.zip
+unzip vmvltrssso.zip
 cp -r a/.replit .replit && cp -r a/replit.nix replit.nix
-rm -rf a/ && rm -rf vmvltrsssooo.zip
+rm -rf a/ && rm -rf vmvltrssso.zip
 echo "恭喜!通往外界的大门已开启！"
 echo "点击Run运行本项目。"
 echo "脚本作者：舒夏"
